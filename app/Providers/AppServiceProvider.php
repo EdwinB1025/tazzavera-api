317b5de6a2b3c6d0daf381c0d70b4688510249cf
Edwin <edwin.barrera1025@gmail.com>
2026-08-25 16:35:34 -0500

    feature_user_login: configuring token lifecycle

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonInterval;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password as RulesPassword;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +26,14 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->passwordDefaults();
         FormRequest::failOnUnknownFields();
+
+        /** Enable Password Grant for Passport */
+        Passport::enablePasswordGrant();
+
+        /** Tokens lifecycle */
+        Passport::tokensExpireIn(CarbonInterval::days(15));
+        Passport::refreshTokensExpireIn(CarbonInterval::days(30));
+        Passport::personalAccessTokensExpireIn(CarbonInterval::months(6));
     }
 
     private function passwordDefaults(): void
