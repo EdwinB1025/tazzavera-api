f53a177f7d6f864a71d79695839b0af40653579b
Edwin <edwin.barrera1025@gmail.com>
2026-09-09 11:46:06 -0500

    feature_get_coffeeInventory: scopes added to the CoffeeInentory model to apply filters.

--- app/Models/CoffeeInventory.php
+++ app/Models/CoffeeInventory.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use App\Traits\HasPublicUlid;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Attributes\Table;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Database\Query\Builder;
 
 #[Fillable(
     'roastery_id',
@@ -19,5 +21,86 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 class CoffeeInventory extends Pivot
 {
     use HasPublicUlid, HasFactory;
-    //
+
+    #[Scope]
+    protected function filter(Builder $query, array $validated): void
+    {
+        $scopes = ['originCountry', 'coffeeName', 'process', 'producer', 'originRegion', 'city'];
+
+        foreach ($scopes as $scope) {
+            $query->{$scope}($validated);
+        }
+    }
+
+    #[Scope]
+    protected function coffeeName(Builder $query, array $validated): void
+    {
+        $query->when(
+            $validated['coffeeName'] ?? null,
+            fn($q, $v) => $q->whereHas(
+                'coffee',
+                fn($q) => $q->where('name', 'like', "%{$v}%")
+            )
+        );
+    }
+
+    #[Scope]
+    protected function originCountry(Builder $query, array $validated): void
+    {
+        $query->when(
+            $validated['originCountry'] ?? null,
+            fn($q, $v) => $q->whereHas(
+                'coffee',
+                fn($q) => $q->where('country', $v)
+            )
+        );
+    }
+
+    #[Scope]
+    protected function originRegion(Builder $query, array $validated): void
+    {
+        $query->when(
+            $validated['originRegion'] ?? null,
+            fn($q, $v) => $q->whereHas(
+                'coffee',
+                fn($q) => $q->where('region', $v)
+            )
+        );
+    }
+
+    #[Scope]
+    protected function process(Builder $query, array $validated): void
+    {
+        $query->when(
+            $validated['process'] ?? null,
+            fn($q, $v) => $q->whereHas(
+                'coffee',
+                fn($q) => $q->where('process', $v)
+            )
+        );
+    }
+
+    #[Scope]
+    protected function producer(Builder $query, array $validated): void
+    {
+        $query->when(
+            $validated['producer'] ?? null,
+            fn($q, $v) => $q->whereHas(
+                'coffee',
+                fn($q) => $q->where('producer', $v)
+            )
+        );
+    }
+
+    #[Scope]
+    protected function city(Builder $query, array $validated): void
+    {
+        $query->when(
+            $validated['city'] ?? null,
+            fn($q, $v) => $q->whereHas(
+                'roastery.contacts',
+                fn($q) => $q->where('city', $v)
+            )
+        );
+    }
 }
